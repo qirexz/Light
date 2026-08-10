188feccf4b2922b765704e143df3a6249535a5ce
qirexz <142836585+qirexz@users.noreply.github.com>
2026-08-11 02:28:02 +0300

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
